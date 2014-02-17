d45d64413d5bebd759c8810f35397ccae0d33a7c
Julie Kang <julie.kang@stanfordalumni.org>
2014-02-16 20:57:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+CuteBot, a cuteness-friendly IRC bot
+====================================
+Written in node.js, with love
+
+To run: 
+
+Fill in the BLANKs in lines 6, 9, 10 with your Google developer API key, IRC server, and channel(s) of choice.  Then <code>node server.js</code>, and you should be good to go!
+
+Feedback welcome: juliekang@alumni.stanford.edu
---
+++ server.js
@@ -0,0 +1,56 @@
+var irc = require('irc');
+var images_search_client = require('google-images');
+var request = require('request');
+var fs, configurationFile;
+ 
+configurationFile = 'configuration.json.sample';
+fs = require('fs');
+ 
+var configuration = JSON.parse(
+    fs.readFileSync(configurationFile)
+);
+
+// You will need to supply your own Google API Key
+var GAPI_KEY = configuration.googleAPIKey;
+
+// You will need to supply your own IRC server and desired channel(s)
+var bot = new irc.Client(configuration.ircHost, 'cutebot', {
+    channels: configuration.ircChannels,
+    port: configuration.ircPort,
+    debug: true,
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+bot.addListener('message', function(from, to, message) {
+  message = message.trim().toLowerCase();
+
+  if (message.indexOf('cutebot') !== -1) {
+    if (message === 'cutebot help') {
+      bot.say(to, 'I am cutebot. Say my name and I will give you cute.');
+    } else {
+      var animals = ['dog', 'cat', 'puppy', 'kitten', 'otter', 'owl', 'pig', 'piglet', 'benedict cumberbatch', 'seal', 'duckling', 'chick', 'corgi'];
+      animal = animals[getRandomInt(0, animals.length - 1)];
+      images_search_client.search(
+        'cute ' + animal, {
+          page: getRandomInt(1, 10), 
+          callback: function(status, images) {
+            url = images[0].url;
+            //console.log(url);
+
+          var payload = { longUrl: url };
+          request.post({
+            url: 'https://www.googleapis.com/urlshortener/v1/url?key=' + GAPI_KEY, 
+            json:payload
+          }, function(error, response, body) {
+            bot.say(to, "Here!  Take this " + animal + "!");
+            console.log(JSON.stringify(body));
+            bot.say(to, body.id);
+          });
+        }
+      });
+    }
+  }
+});
