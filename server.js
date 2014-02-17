82caa618e9616a37fd81a26510520922fe1ab1e9
Julie Kang <julie.kang@stanfordalumni.org>
2014-02-17 12:50:48 -0800

    More refactoring and readability cleanup.

--- server.js
+++ server.js
@@ -1,63 +1,57 @@
 "use strict";
 
-var util = require('./util');
-var irc = require('irc');
-var images_search_client = require('google-images');
-var request = require('request');
-var redis = require("redis"),
-    redisClient = redis.createClient();
-var fs = require('fs'), 
-    configurationFile = 'configuration.json',
-    configuration = JSON.parse(
-      fs.readFileSync(configurationFile)
-    );
-// You will need to supply your own Google API Key
-var GAPI_KEY = configuration.googleAPIKey;
+var util = require("./util"),
+    irc = require("irc"),
+    imageSearchClient = require("google-images"),
+    request = require("request"),
+    redis = require("redis"),
+    redisClient = redis.createClient(),
+    fs = require("fs");
 
-// You will need to supply your own IRC server and desired channel(s)
-var bot = new irc.Client(configuration.ircHost, 'cutebot', {
-    channels: configuration.ircChannels,
-    port: configuration.ircPort,
+var configFile = "config.json",
+    config = JSON.parse(
+      fs.readFileSync(configFile)
+    );
+
+var bot = new irc.Client(config.ircHost, "cutebot", {
+    channels: config.ircChannels,
+    port: config.ircPort,
     debug: true,
 });
 
+bot.addListener("message", function(from, to, message) {
+  var animal, page, hitNum, cacheKey, cachedValue;
 
-
-bot.addListener('message', function(from, to, message) {
-  message = message.trim().toLowerCase();
-
-  if (message.indexOf('cutebot') !== -1) {
-    if (message === 'cutebot help') {
-      bot.say(to, 'I am cutebot. Say my name and I will give you cute.');
+  message = util.normalizeMessage(message);
+  if (message.indexOf("cutebot") !== -1) {
+    if (message === "cutebot help") {
+      bot.say(to, "I am cutebot. Say my name and I will give you cute.");
     } else {
-      var animals = configuration.supportedAnimals;
-      var index = util.getRandomInt(0, animals.length - 1);
-      var page = util.getRandomInt(1, configuration.numResultPages);
-      // There are 3 results per page from the (deprecated) Google Image Search API.
-      var hitNum = util.getRandomInt(0, 2);
-      var animal = animals[index];
-
-      var cacheKey = animal + "-" + page + "-" + hitNum;
-      var cachedValue;
+      animal = getRandomAnimal();
+      page = getRandomPage();
+      hitNum = getRandomResultIndex();
+      cacheKey = getCacheKey(animal, page, hitNum);
 
       redisClient.get(cacheKey, function(err, reply) {
         cachedValue = reply;
         if (cachedValue) {
-          console.log("Found value \"" + cachedValue + "\" for key \"" + cacheKey + "\"");
+          console.log("Found " + cachedValue + " for key:" + cacheKey);
           bot.say(to, "Hey " + from + "! Take this " + animal + "! " + cachedValue);
         } else {
-          console.log("CACHE MISS!!! for key \"" + cacheKey + "\"");
-          images_search_client.search(
-            'cute ' + animal, {
+          console.log("CACHE MISS for key:" + cacheKey);
+          imageSearchClient.search(
+            "cute " + animal, {
               page: page, 
               callback: function(status, images) {
                 var payload = { longUrl: images[hitNum].url };
                 request.post({
-                  url: 'https://www.googleapis.com/urlshortener/v1/url?key=' + GAPI_KEY, 
+                  url: "https://www.googleapis.com/urlshortener/v1/url?key=" + config.googleAPIKey, 
                   json:payload
                 }, function(error, response, body) {
                   var url = body.id;
                   redisClient.set(cacheKey, url);
+                  redisClient.expire(cacheKey, config.cacheExpiry);
+                  console.log("Caching " + url + " for key: " + cacheKey);
                   bot.say(to, "Hey " + from + "! Take this " + animal + "! " + url);
                 });
               }
@@ -68,3 +62,20 @@ bot.addListener('message', function(from, to, message) {
     }
   }
 });
+
+function getRandomAnimal() {
+  return config.animals[util.getRandomInt(0, config.animals.length - 1)];
+};
+
+function getRandomPage() {
+  return util.getRandomInt(1, config.numResultPages);
+}
+
+function getRandomResultIndex() {
+  // There are 3 results per page from the (deprecated) Google Image Search API.
+  return util.getRandomInt(0, 2);
+}
+
+function getCacheKey(animal, page, hitNum) {
+  return animal + "-" + page + "-" + hitNum;
+};
--- util.js
+++ util.js
@@ -1,3 +1,7 @@
 exports.getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
+
+exports.normalizeMessage = function(message) {
+  return message.trim().toLowerCase();
+};
