eaa21a36977300d5ee57b014b577913912c25d76
Julie Kang <julie.kang@stanfordalumni.org>
2014-02-17 11:36:39 -0800

    Switch to using redis for the cache.

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
+"use strict";
+
 var irc = require('irc');
 var images_search_client = require('google-images');
 var request = require('request');
-var fs, configurationFile;
- 
-var CACHE = {};
-
-configurationFile = 'configuration.json';
-fs = require('fs');
- 
-var configuration = JSON.parse(
-    fs.readFileSync(configurationFile)
-);
-
+var redis = require("redis"),
+    redisClient = redis.createClient();
+var fs = require('fs'), 
+    configurationFile = 'configuration.json',
+    configuration = JSON.parse(
+      fs.readFileSync(configurationFile)
+    );
 // You will need to supply your own Google API Key
 var GAPI_KEY = configuration.googleAPIKey;
 
@@ -34,28 +32,38 @@ bot.addListener('message', function(from, to, message) {
       bot.say(to, 'I am cutebot. Say my name and I will give you cute.');
     } else {
       var animals = ['dog', 'cat', 'puppy', 'kitten', 'otter', 'owl', 'pig', 'piglet', 'benedict cumberbatch', 'seal', 'duckling', 'chick', 'corgi'];
-      index = getRandomInt(0, animals.length - 1);
-      page = getRandomInt(1, 10);
-      animal = animals[index];
-      if (!CACHE[animal + page]) {
-        images_search_client.search(
-          'cute ' + animal, {
-            page: page, 
-            callback: function(status, images) {
-              var payload = { longUrl: images[0].url };
-              request.post({
-                url: 'https://www.googleapis.com/urlshortener/v1/url?key=' + GAPI_KEY, 
-                json:payload
-              }, function(error, response, body) {
-                CACHE[animal + page] = body.id;
-                bot.say(to, "Hey " + from + "! Take this " + animal + "! " + body.id);
-              });
+      var index = getRandomInt(0, animals.length - 1);
+      var page = getRandomInt(1, 10);
+      var animal = animals[index];
+
+      var cacheKey = animal + page;
+      var cachedValue;
+
+      redisClient.get(cacheKey, function(err, reply) {
+        cachedValue = reply;
+        if (cachedValue) {
+          console.log("Found value \"" + cachedValue + "\" for key \"" + cacheKey + "\"");
+          bot.say(to, "Hey " + from + "! Take this " + animal + "! " + cachedValue);
+        } else {
+          console.log("CACHE MISS!!! for key \"" + cacheKey + "\"");
+          images_search_client.search(
+            'cute ' + animal, {
+              page: page, 
+              callback: function(status, images) {
+                var payload = { longUrl: images[0].url };
+                request.post({
+                  url: 'https://www.googleapis.com/urlshortener/v1/url?key=' + GAPI_KEY, 
+                  json:payload
+                }, function(error, response, body) {
+                  var url = body.id;
+                  redisClient.set(cacheKey, url);
+                  bot.say(to, "Hey " + from + "! Take this " + animal + "! " + url);
+                });
+              }
             }
-          }
-        );
-      } else {
-        bot.say(to, "Hey " + from + "! Take this " + animal + "! " + CACHE[animal + page]);
-      }
+          );
+        }
+      });
     }
   }
 });
